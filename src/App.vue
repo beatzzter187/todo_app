253165399553c3460658416e4440a183200aa301
Simon Read <s.read@walz.de>
2023-02-15 09:52:42 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import TodoItem from "./components/TodoItem.vue";
+// https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fc5%2F78%2F1d%2Fc5781d72c1298dc869b74702b4ee42a0.png&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F520869513146671442%2F&tbnid=PZkIWgyWGskinM&vet=12ahUKEwjIscTbs5L9AhUO47sIHd4hARkQMygBegUIARDDAQ..i&docid=MrqpUNS3WToXZM&w=800&h=600&q=todo%20website&client=firefox-b-e&ved=2ahUKEwjIscTbs5L9AhUO47sIHd4hARkQMygBegUIARDDAQ
+</script>
+
+<style>
+.title {
+  background-color: #6c39cb;
+  text-align: center;
+  font-size: 40px;
+  font-weight: 800;
+  font-style: initial;
+  margin: 40px;
+  margin-top: 70px;
+  padding: 17px;
+}
+.input input {
+  background-color: aqua;
+  position: static;
+  width: 10px;
+  height: 5px;
+}
+</style>
+
+<template>
+  <div class="title rounded">
+    <h3>Todo App</h3>
+  </div>
+  <div class="input">
+    <input type="text" placeholder="test" />
+  </div>
+  <div class="grid place-items-center">
+    <div
+      class="bg-slate-200 border-slate-200 border-r border-b p-7 m-5 rounded w-1/2"
+    >
+      <div>
+        <TodoItem>Dishes</TodoItem>
+        <TodoItem>Dishes</TodoItem>
+        <TodoItem>Dishes</TodoItem>
+        <TodoItem>Dishes</TodoItem>
+      </div>
+      <div class="flex justify-center border-gray-700 mt-1">
+        <button
+          type="button"
+          class="absolute inline-block px-10 py-2 bg-purple-600 text-white font-bold text-base leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          <p class="">+</p>
+          New Task
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,57 @@
+<script setup>
+const todoName = "Dishes";
+</script>
+<style>
+input {
+  position: absolute;
+  opacity: 0;
+  height: 0;
+  width: 0;
+}
+label {
+  position: relative;
+  display: block;
+  padding-left: 30px;
+  cursor: pointer;
+}
+label:before {
+  content: "";
+  position: absolute;
+  top: 50%;
+  margin-top: -10px;
+  left: 0;
+  display: block;
+  height: 20px;
+  width: 20px;
+  background-color: #aa8aeb;
+  border-radius: 3px;
+}
+label:hover:before {
+  background-color: #be68da;
+}
+label:after {
+  content: "";
+  position: absolute;
+  left: 7px;
+  top: 1px;
+  width: 5px;
+  height: 10px;
+  border: solid #333;
+  border-width: 0 3px 3px 0;
+  transform: rotate(45deg);
+  opacity: 0;
+}
+input:checked + label:after {
+  opacity: 1;
+}
+.todo {
+  font-size: 20px;
+  font-weight: 650;
+}
+</style>
+
+<template>
+  <input type="checkbox" id="checkbox" />
+  <label for="checkbox" class="todo">{{ todoName }}</label>
+  <p></p>
+</template>
